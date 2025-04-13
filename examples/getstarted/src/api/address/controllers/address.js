ec7247138258353ef0e2c8d4916b029eec17bc93
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2025-04-13 13:11:53 -0700

    Add new functionality to Strapi
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/team-dev-docs/strapi?shareId=XXXX-XXXX-XXXX-XXXX).

--- examples/getstarted/src/api/address/controllers/address.js
+++ examples/getstarted/src/api/address/controllers/address.js
@@ -6,4 +6,24 @@ module.exports = createCoreController('api::address.address', {
 
     ctx.body = await this.sanitizeOutput(results, ctx);
   },
+
+  async create(ctx) {
+    const { data } = await strapi.service('api::address.address').create(ctx.request.body);
+
+    ctx.body = await this.sanitizeOutput(data, ctx);
+  },
+
+  async update(ctx) {
+    const { id } = ctx.params;
+    const { data } = await strapi.service('api::address.address').update(id, ctx.request.body);
+
+    ctx.body = await this.sanitizeOutput(data, ctx);
+  },
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+    const { data } = await strapi.service('api::address.address').delete(id);
+
+    ctx.body = await this.sanitizeOutput(data, ctx);
+  },
 });
